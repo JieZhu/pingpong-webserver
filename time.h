6e93fce19011e9fc35ac85190879adf8b062fca0
Li Shen <li.shen@rice.edu>
2014-09-17 23:12:57 -0500

    modified computing method for latency

--- time.h
+++ time.h
@@ -1,5 +1,5 @@
 #include <sys/time.h>
 
 void set_timestamp(char*, struct timeval*);
-void get_timestamp(char*, struct timeval*);
+void get_timestamp(struct timeval*);
 unsigned long get_latency(struct timeval*, struct timeval*);
